e405652b98422d1e0723540d596441ce6d2f67b0
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-18 10:42:24 -0600

    refactor: Refactorizando ShoppingPage creando hook

---
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from 'react';
+import { Product, ProductInCart } from '../interfaces/interfaces';
+
+export const useShoppingCart = () => {
+  const [shoppingCart, setShoppingCart] = useState<{
+    [key: string]: ProductInCart;
+  }>({});
+
+  const onProductCountChange = ({
+    count,
+    product,
+  }: {
+    count: number;
+    product: Product;
+  }) => {
+    setShoppingCart((oldShoppingCart) => {
+      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
+        ...product,
+        count: 0,
+      };
+
+      if (Math.max(productInCart.count + count, 0) > 0) {
+        productInCart.count += count;
+
+        return {
+          ...oldShoppingCart,
+          [product.id]: productInCart,
+        };
+      }
+
+      //Hay que borrar el producto (count < 0)
+      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+      return rest;
+
+      // if (count === 0) {
+      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+      //   return rest;
+      // }
+      // return {
+      //   ...oldShoppingCart,
+      //   [product.id]: { ...product, count },
+      // };
+    });
+  };
+
+  return {
+    shoppingCart,
+
+    onProductCountChange,
+  };
+};

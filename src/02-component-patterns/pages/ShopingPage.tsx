10c4908a91835dfea6b27d5edfa27aa4c081238b
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-23 09:35:12 -0600

    feat: Preparacion del juego de componentes

--- src/02-component-patterns/pages/ShopingPage.tsx
+++ src/02-component-patterns/pages/ShopingPage.tsx
@@ -19,31 +19,16 @@ export const ShopingPage = () => {
 
       <ProductCard
         product={product}
-        className="bg-dark text-white"
         initialValues={{
           count: 4,
-          maxCount: 10,
+          // maxCount: 10,
         }}
       >
-        {({ reset, isMaxCountReached, maxCount, count, increaseBy }) => (
+        {() => (
           <>
-            <ProductImage
-              className="custom-image"
-              style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-            />
-            <ProductTitle className="text-bold" />
-            <ProductButtons className="custom-buttons" />
-
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}>-2</button>
-
-            {/* Si no se llega al isMaxCount, ocultar */}
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(2)}>+2</button>
-            )}
-            <span>
-              {count} - {maxCount}
-            </span>
+            <ProductImage />
+            <ProductTitle />
+            <ProductButtons />
           </>
         )}
       </ProductCard>

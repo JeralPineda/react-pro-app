fec066671e5dd17616c662ec7d0179bdf37cd4b2
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-24 08:44:26 -0600

    fix: Expandir funcionalidad de nuestro custom hook

--- src/03-forms/hooks/useForm.ts
+++ src/03-forms/hooks/useForm.ts
@@ -10,12 +10,24 @@ export const useForm = <T>(initState: T) => {
     }));
   };
 
+  const resetForm = () => {
+    setFormData({ ...initState });
+  };
+
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
   return {
     //Properties
     ...formData,
     formData,
 
     //Methods
+    isValidEmail,
     onChange,
+    resetForm,
   };
 };
--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -3,7 +3,16 @@ import { useForm } from '../hooks/useForm';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
-  const { formData, name, email, password1, password2, onChange } = useForm({
+  const {
+    formData,
+    name,
+    email,
+    password1,
+    password2,
+    onChange,
+    resetForm,
+    isValidEmail,
+  } = useForm({
     name: '',
     email: '',
     password1: '',
@@ -27,7 +36,9 @@ export const RegisterPage = () => {
           name="name"
           value={name}
           onChange={onChange}
+          className={`${name.trim().length <= 0 && 'has-error'}`}
         />
+        {name.trim().length <= 0 && <span>Este campo es necesario</span>}
 
         <input
           type="email"
@@ -35,7 +46,9 @@ export const RegisterPage = () => {
           name="email"
           value={email}
           onChange={onChange}
+          className={`${!isValidEmail(email) && 'has-error'}`}
         />
+        {!isValidEmail(email) && <span>Email no es valido</span>}
 
         <input
           type="password"
@@ -43,7 +56,12 @@ export const RegisterPage = () => {
           name="password1"
           value={password1}
           onChange={onChange}
+          className={`${password1.trim().length <= 0 && 'has-error'}`}
         />
+        {password1.trim().length <= 0 && <span>Este campo es necesario</span>}
+        {password1.trim().length < 6 && password1.trim().length > 0 && (
+          <span>La contraseña tiene que tener 6 caracteres</span>
+        )}
 
         <input
           type="password"
@@ -51,9 +69,17 @@ export const RegisterPage = () => {
           name="password2"
           value={password2}
           onChange={onChange}
+          className={`${password2.trim().length <= 0 && 'has-error'}`}
         />
+        {password2.trim().length <= 0 && <span>Este campo es necesario</span>}
+        {password2.trim().length > 0 && password1 !== password2 && (
+          <span>Las contraseñas deben de ser iguales</span>
+        )}
 
         <button type="submit">Create</button>
+        <button type="button" onClick={resetForm}>
+          Reset Form
+        </button>
       </form>
     </div>
   );

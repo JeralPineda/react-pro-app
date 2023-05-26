24e4a1dc06b65eba0b89970832dd92158a1cd083
Jeral Pineda <jpineda@banhcafe.hn>
2023-05-26 08:24:39 -0600

    feat: Obtener informacion del formulario

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,21 +1,53 @@
+import { useFormik } from 'formik';
+
 export const FormikBasicPage = () => {
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div>
       <h1>Formik Basic Tutorial</h1>
 
-      <form>
+      <form onSubmit={handleSubmit} noValidate>
         <label htmlFor="firstName">First Name</label>
-        <input type="text" placeholder="firstName" />
-        <span>First name is required</span>
+        <input
+          type="text"
+          name="firstName"
+          // onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.firstName}
+        />
+        {/* {touched.firstName && errors.firstName && (
+          <span>{errors.firstName}</span>
+        )} */}
 
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" placeholder="lastName" />
-        <span>Last name is required</span>
+        <input
+          type="text"
+          name="lastName"
+          // onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.lastName}
+        />
+        {/* {touched.lastName && errors.lastName && <span>{errors.lastName}</span>} */}
 
         <label htmlFor="email">Email Address</label>
-        <input type="email" placeholder="email" />
-        <span>Email is required</span>
-        <span>Check for an valid email format</span>
+        <input
+          type="email"
+          name="email"
+          // onBlur={handleBlur}
+          onChange={handleChange}
+          value={values.email}
+        />
+        {/* {touched.email && errors.email && <span>{errors.email}</span>} */}
 
         <button type="submit">Submit</button>
       </form>

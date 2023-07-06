f0fe9b90ebad389c4d21a3a7f1fd72fd4ce2ce11
Jeral Pineda <jpineda@banhcafe.hn>
2023-07-06 10:13:13 -0600

    fix: Trabajando con los selects de manera dinamica

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Form, Formik } from 'formik';
 import formJson from '../data/custom-form.json';
-import { MyTextInput } from '../components';
+import { MySelect, MyTextInput } from '../components';
 
 const initialValues: { [key: string]: any } = {};
 
@@ -23,16 +23,31 @@ export const DynamicForm = () => {
       >
         {() => (
           <Form>
-            {formJson.map(({ type, name, placeholder, label }) => {
-              return (
-                <MyTextInput
-                  key={name}
-                  type={type as any}
-                  label={label}
-                  name={name}
-                  placeholder={placeholder}
-                />
-              );
+            {formJson.map(({ type, name, placeholder, label, options }) => {
+              if (type === 'input' || type === 'password' || type === 'email') {
+                return (
+                  <MyTextInput
+                    key={name}
+                    type={type as any}
+                    label={label}
+                    name={name}
+                    placeholder={placeholder}
+                  />
+                );
+              } else if (type === 'select') {
+                return (
+                  <MySelect key={name} label={label} name={name}>
+                    <option value="">Select an option</option>
+                    {options?.map(({ id, label }) => (
+                      <option key={id} value={label}>
+                        {label}
+                      </option>
+                    ))}
+                  </MySelect>
+                );
+              }
+
+              throw new Error(`El type: ${type}, no es soportado`);
             })}
             <button type="submit">Submit</button>
           </Form>
